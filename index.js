c535fcae89a7a3b0035a3ed48aecf1373e81d3d9
Deepak Verma <deepakverma@abaq.us>
2024-07-31 00:41:28 +0530

    crud operations API

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const index = fs.readFileSync("index.html", 'utf-8');
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const productData = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const newDataAdd = 
+{
+    "id": 21,
+    "title": "IPhone Print",
+    "price": 30.99,
+    "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    "category": "mens clothing",
+    "image": "",
+    "rating": {
+      "rate": 3.6,
+      "count": 145
+    }
+  }
 
 const express = require("express");
 const morgan = require("morgan")
@@ -11,48 +24,53 @@ server.use(express.json())
 server.use(express.static('public'))
 server.use(morgan('dev')) //third party middle ware
 
-
-// middle-ware
-// server.use((req, res, next) => {
-//     console.log(req.method, req.id, req.hostname)
-//     next()
-// })
-
-const auth = (req, res, next) => { // auth middle ware logic
-    console.log(req.query)
-    if (req.query.password) {
-        next()
-    } else {
-        res.sendStatus(401)
-    }
-    next()
-}
-
-server.use(auth)
-
-
 //API-ENDPOINT-ROUTE
-server.get('/product/:id',auth, (req, res) => {
-    res.json({ type: "GET" })
+//products
+
+//READ get/products 
+server.get('/products', (req, res) => {
+    res.json(productData)
 })
 
-server.post('/', (req, res) => {
-    res.json({ type: "POST" })
-})
-server.put('/', (req, res) => {
-    res.json({ type: "PUT" })
-})
-server.delete('/', (req, res) => {
-    res.json({ type: "DELETE" })
+server.get('/products/:id', (req, res) => {
+    console.log(req)
+    const id = +req.params.id
+    const singleProduct = productData.find((e)=>(e.id===id))
+    res.json(singleProduct)
 })
 
-server.get('/demo', (req, res) => {
-    res.status(503).send('<h1>service not found</h1>')
-    // res.sendStatus(503)
-    // res.send("hello")
-    // res.json(data)
+//create API
+server.post('/product-add', (req, res) => {
+    productData.push(newDataAdd);
+    res.json(newDataAdd);
 })
 
+//update put
+server.put('/products/:id', (req, res) => {
+    console.log(req)
+    const id = +req.params.id
+    const ProductIndex = productData.findIndex((e)=>(e.id===id))
+    productData.splice(ProductIndex,1,{...req.body,id:id})
+    res.status(201).json()
+})
+
+//Patch 
+server.patch('/products/:id', (req, res) => {
+    console.log(req)
+    const id = +req.params.id
+    const productIndex = productData.findIndex((e)=>(e.id===id))
+    const ogProd = productData[productIndex]
+    productData.splice(productIndex,1,{...ogProd,...req.body})
+    res.status(201).json()
+})
+
+
+server.delete('/products/:id', (req, res) => {
+    const id = +req.params.id
+    const ProductIndex = productData.findIndex((e)=>(e.id===id))
+    productData.splice(ProductIndex,1)
+    res.status(201).json()
+})
 
 
 server.listen(8080, () => {
