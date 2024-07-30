f1086459ebb98e0921ec2532cfd1144994037485
Deepak Verma <deepakverma@abaq.us>
2024-07-30 22:09:55 +0530

    middleware and endpoints

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-// import {sum,diff} from "./lib.js"
-// const express = require('express')
-// const fs = require('fs');
-// fs.readFile("demo.txt",'utf-8',(err,txt)=>{console.log(txt)})
-// console.log("hello world")
-// // console.log(sum(4,3),diff(4,3))
-// const server = express();
-// server.listen(8080)
-
-// -----------------------------------------
-
-const http = require('http');
 const fs = require('fs');
-
 const index = fs.readFileSync("index.html", 'utf-8');
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const singleProduct = data;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if (req.url.startsWith('/product')) {
-        let len = req.url.split('/').length;
-        console.log(len)
-        const id = req.url.split('/')[2]
-        const showProduct = singleProduct.find(i => i.id === (+id))
-        console.log(showProduct)
-        res.setHeader('Content-Type', 'text/html')
-        let product = index.replace("**title**", showProduct.title)
-            .replace("**url**", showProduct.image)
-            .replace(" **price**", showProduct.price)
-        res.end(product)
-        return
+const express = require("express");
+const morgan = require("morgan")
+const server = express()
 
+//boby parser
+server.use(express.json())
+server.use(express.static('public'))
+server.use(morgan('dev')) //third party middle ware
+
+
+// middle-ware
+// server.use((req, res, next) => {
+//     console.log(req.method, req.id, req.hostname)
+//     next()
+// })
+
+const auth = (req, res, next) => { // auth middle ware logic
+    console.log(req.query)
+    if (req.query.password) {
+        next()
+    } else {
+        res.sendStatus(401)
     }
-    switch (req.url) {
-        case '/':
-            res.setHeader('Content-Type', 'text/html');
-            res.end(index);
-            break
-        case '/api':
-            res.setHeader('Content-Type', 'application/json')
-            res.end(data)
-            break
-        // case '/product':
-        //     res.setHeader('Content-Type', 'text/html')
-        //     let product = index.replace("**title**", singleProduct.title).replace("**url**", singleProduct.image).replace(" **price**", singleProduct.price)
-        //     res.end(product)
-        //     break
-        default:
-            res.writeHead(404, "Not Found")
-            res.end()
-    }
-    console.log("server started ")
-    // res.setHeader('Dummy', 'dummy/value');
+    next()
+}
 
+server.use(auth)
+
+
+//API-ENDPOINT-ROUTE
+server.get('/product/:id',auth, (req, res) => {
+    res.json({ type: "GET" })
 })
 
-server.listen(8080)
+server.post('/', (req, res) => {
+    res.json({ type: "POST" })
+})
+server.put('/', (req, res) => {
+    res.json({ type: "PUT" })
+})
+server.delete('/', (req, res) => {
+    res.json({ type: "DELETE" })
+})
+
+server.get('/demo', (req, res) => {
+    res.status(503).send('<h1>service not found</h1>')
+    // res.sendStatus(503)
+    // res.send("hello")
+    // res.json(data)
+})
+
+
+
+server.listen(8080, () => {
+    console.log("server started")
+})
